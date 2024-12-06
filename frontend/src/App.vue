6ece720c4931e681eaef5c16e9b3bae8a1f118c3
Henry Campos <henry.camposnavarro@ucr.ac.cr>
2024-12-05 18:21:09 -0600

    Se sube la estructura inicial del proyecto en frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <CoffeeMachine />
+  </div>
+</template>
+
+<script>
+import CoffeeMachine from "./components/CoffeeMachine.vue";
+
+export default {
+  name: "App",
+  components: {
+    CoffeeMachine,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+}
+</style>
+
